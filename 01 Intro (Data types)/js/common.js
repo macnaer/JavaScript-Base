65f69a2011283607938939f79daf371d7f45b05a
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-02-21 20:56:49 +0200

    01 Intro (Dtata types)

---
+++ 01 Intro (Data types)/js/common.js
@@ -0,0 +1,84 @@
+/*console.log("Hello world!");
+
+var age = 20;
+console.log("Age = ", age);
+console.log(typeof age);
+
+var value = 3.14;
+console.log(value);
+console.log(typeof value);
+
+var name = "Bill";
+console.log(name);
+console.log(typeof name);
+
+name = 10;
+console.log(name);
+
+/*console.error("Test error");
+console.warn("Test alert");
+// console.log("Age = " + age);
+
+var exit = false;
+
+console.log(exit);
+console.log(typeof exit);
+
+var c;
+
+console.log(c);
+console.log(typeof c);
+
+/*var d = NULL;
+
+console.log(d);
+console.log(typeof d);
+
+var arr = [1,234,345,3454,646, 324.324, "test", [1,2,4,"ereewerw"] ];
+console.log(arr);
+console.log(typeof arr);
+
+
+var Person = {
+    name: "jason",
+    surname: "Robson",
+    age: 35,
+    seyHello: function(){
+        console.log(this.name , "says hello");
+    }
+}
+
+console.log(typeof Person);
+console.log(Person);
+Person.name = "David";
+console.log(Person);
+Person.seyHello();
+console.log(typeof Person.seyHello);*/
+
+//=============================================================================
+
+var a = 5.2, b = 10, c = 2;
+
+
+var result = 0;
+
+//result = a + b;
+//result = a - b;
+//result = a * b;
+//result = a / b;
+//result = a / b;
+
+result = (2 + 2) * 2;
+
+
+console.log(result);
+console.log(typeof result);
+
+//================================================================================
+
+/*document.write(a);
+alert("Alert");*/
+
+var day = prompt("Enter day!");
+document.write(day);
+document.write(typeof day);
