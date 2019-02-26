a47118a320682b8adac3dce67d581e50764b5fd4
master <master@rivne.itstep.org>
2019-02-26 19:21:13 +0200

    Switch and get data from input

---
+++ 02 if else/js/common.js
@@ -0,0 +1,42 @@
+console.log("Works");
+
+// var a = 203, b = 20;
+
+// if (a > b){
+//     console.log(a, " > ", b);
+// } 
+// else if(a < b){
+//     console.log(a, " < ", b);
+// }
+// else {
+//     console.log(a, " = ", b);
+// }
+
+//===================================
+
+/*var m = 5, n = 5, k = 5;
+
+if (m > n && m > k){
+    console.log(" m > n and m > k");
+}
+else if (n > m && n > kn > m && n > k){
+    console.log(" n > m and n > k");
+}
+else if (k > m && k > n){
+    console.log(" k > n and k > m");
+}
+else{
+    console.log(" k = n = m");
+}*/
+
+//================================
+
+function GetData() {
+    let usrname = document.querySelector(".username").value;
+    let password = document.querySelector(".password").value;
+
+
+    console.log(usrname);
+    console.log(password);
+
+}
